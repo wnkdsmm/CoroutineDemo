dca8ea18d54535210e81e1a760e667d896fa106e
wnkdsmm <lwjfkg@mail.ru>
2024-09-26 14:40:24 +0700

    CoroutineDemo-commit1

---
+++ app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.hfad.coroutinedemo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.hfad.coroutinedemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
