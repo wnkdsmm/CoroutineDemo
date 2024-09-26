f33d8761cb63767125fc58ffdeface6303bd03be
wnkdsmm <lwjfkg@mail.ru>
2024-09-26 15:22:05 +0700

    CoroutineDemo-commit2

--- app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
@@ -9,18 +9,39 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.SeekBar
 import com.hfad.coroutinedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    count = progress
+                    binding.countText.text = "${count} coroutines"
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+                }
+            })
 
     }
+
+    fun launchCoroutines(view: View) {}
 
 }
